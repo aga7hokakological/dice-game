4f83e912659160fbcc6e0bac70b888449bc3c448
aga7hokakological <iamsaurabhmandavkar@gmail.com>
2022-01-25 23:40:27 +0530

    test added

--- programs/dice-game/src/lib.rs
+++ programs/dice-game/src/lib.rs
@@ -13,25 +13,29 @@ pub mod dice_game {
         Ok(())
     }
 
-    pub fn play_game(ctx: Context<PlayGame>, placed_bet: u64) -> {
+    pub fn play_game(ctx: Context<PlayGame>, placed_bet: u64) -> ProgramResult {
         let player = &mut ctx.accounts.player;
         let game_wallet = &mut ctx.accounts.game_wallet;
 
-        let game_wallet_lamports = **game_wallet.to_account_info().lamports.borrow();
+        let _game_wallet_lamports = **game_wallet.to_account_info().lamports.borrow();
 
-        let d1 = Clock::get().unwrap().unix_timestamp;
-        let d2 = Clock::get().unwrap().unix_timestamp;
+        let d1 = Clock::get().unwrap().unix_timestamp as u64;
+        let d2 = Clock::get().unwrap().unix_timestamp as u64;
 
         player.dice1 = d1;
         player.dice2 = d2;
         player.result = ((player.dice1 + player.dice2) / 2) as u64;
 
+        msg!("Result: {:?}", player.result);
+
+        let amount = placed_bet * 2;
+
         if player.result == 7 {
-            invoke(
-                &transfer(
+            anchor_lang::solana_program::program::invoke(
+                &anchor_lang::solana_program::system_instruction::transfer(
                     game_wallet.to_account_info().key,
                     ctx.accounts.player.to_account_info().key,
-                    (placed_bet * 2),
+                    amount,
                 ),
                 &[
                     game_wallet.to_account_info(),
@@ -46,9 +50,8 @@ pub mod dice_game {
 }
 
 #[derive(Accounts)]
-#[instruction(bump: u8)]
 pub struct Initialize<'info> {
-    #[account(init, payer=user)]
+    #[account(init, payer=user, space=10240)]
     pub player: Account<'info, Player>,
     #[account(mut)]
     pub user: Signer<'info>,
@@ -56,8 +59,9 @@ pub struct Initialize<'info> {
 }
 
 #[derive(Accounts)]
+// #[instruction(bump: u8)]
 pub struct PlayGame<'info> {
-    #[account(init, seeds=[], bump=8)]
+    #[account(init, seeds=[b"seed".as_ref()], bump=8, payer=game_wallet, space=10240)]
     pub game_wallet: Account<'info, GameWallet>,
     #[account(mut)]
     pub player: Account<'info, Player>,
@@ -65,12 +69,15 @@ pub struct PlayGame<'info> {
     pub system_program: Program<'info, System>
 }
 
+// #[account(signer)]
 #[account]
+#[derive(Default)]
 pub struct GameWallet {
     amount: u64,
 }
 
 #[account]
+#[derive(Default)]
 pub struct Player {
     player_key: Pubkey,
     dice1: u64,
@@ -82,4 +89,4 @@ pub struct Player {
 //     fn space() -> usize {
 //         32 + 8 + 64 + 64 + 64
 //     }
-// }
+// iii}
--- tests/dice-game.js
+++ tests/dice-game.js
@@ -1,57 +1,108 @@
+const assert = require("assert");
 const anchor = require('@project-serum/anchor');
+const BN = anchor.BN;
 
 const { SystemProgram, LAMPORTS_PER_SOL } = anchor.web3;
 
-const main = async() => {
-  console.log("Starting Test..........");
 
-  const provider = anchor.Provider.env();
-  anchor.setProvider(provider);
-  const program = anchor.workspace.DiceGame;
+describe("dice game", () => {
+  // const provider = anchor.Provider.env();
+  // anchor.setProvider(provider);
+
+  anchor.setProvider(anchor.Provider.env());
+  const dice_game = anchor.workspace.Dicegame;
 
   // Create account
   const baseUser = anchor.web3.Keypair.generate();
 
-  // Initialize Test
-  const tx = await program.rpc.initialize({
-    accounts: {
-      baseUser: baseUser.publicKey,
-      user: provider.wallet.publicKey,
-      systemProgram: SystemProgram.programId
-    },
-    signers: [baseUser],
-  });
+  it("Initialize numbers", async () => {
+    // Initialize Test
+    const tx = await dice_game.rpc.initialize({
+      accounts: {
+        baseUser: baseUser.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseUser],
+    });
 
-  console.log("Your transaction signature: ", tx);
+    console.log("Your transaction signature: ", tx);
+  })
 
-
-  let acc = await program.account.baseUser.fetch(baseUser.publicKey);
-  console.log(acc);
+  // let acc = await program.account.baseUser.fetch(baseUser.publicKey);
+  // console.log(acc);
 
   // Create Game Wallet
   const game_wallet = anchor.web3.Keypair.generate();
 
-  // Play Game test
-  let gameTx = await program.rpc.playGame(0.1, {
-    accounts: {
-      baseUser: baseUser.publicKey,
-      game_wallet: game_wallet.publicKey,
-      user: provider.wallet.publicKey,
-      systemProgram: SystemProgram.programId
-    },
-    signers: [baseUser],
-  });
+  it("Play Game", async () => {
+    // Play Game test
+    let gameTx = await dice_game.rpc.playGame(0.1, {
+      accounts: {
+        game_wallet: game_wallet.publicKey,
+        baseUser: baseUser.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+      },
+      signers: [baseUser],
+    });
 
-  console.log("Game transaction signature: ", gameTx);
-};
+    console.log("Game transaction signature: ", gameTx);
+  })
+})
 
-const runMain = async() => {
-  try {
-    await main();
-    process.exit(0);
-  } catch (err) {
-    console.log(err);
-  }
-};
 
-runMain();
+// const main = async() => {
+//   console.log("Starting Test..........");
+
+//   const provider = anchor.Provider.env();
+//   anchor.setProvider(provider);
+//   const program = anchor.workspace.Dicegame;
+
+//   // Create account
+//   const baseUser = anchor.web3.Keypair.generate();
+
+//   // Initialize Test
+//   const tx = await program.rpc.initialize({
+//     accounts: {
+//       baseUser: baseUser.publicKey,
+//       user: provider.wallet.publicKey,
+//       systemProgram: SystemProgram.programId
+//     },
+//     signers: [baseUser],
+//   });
+
+//   console.log("Your transaction signature: ", tx);
+
+
+//   let acc = await program.account.baseUser.fetch(baseUser.publicKey);
+//   console.log(acc);
+
+//   // Create Game Wallet
+//   const game_wallet = anchor.web3.Keypair.generate();
+
+//   // Play Game test
+//   let gameTx = await program.rpc.playGame(0.1, {
+//     accounts: {
+//       baseUser: baseUser.publicKey,
+//       game_wallet: game_wallet.publicKey,
+//       user: provider.wallet.publicKey,
+//       systemProgram: SystemProgram.programId
+//     },
+//     signers: [baseUser],
+//   });
+
+//   console.log("Game transaction signature: ", gameTx);
+// };
+
+// const runMain = async() => {
+//   try {
+//     await main();
+//     process.exit(0);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// runMain();
